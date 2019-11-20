65a99a288c9ec68e92adf6f95d9c23757e35f01e
Yu Hao Ye <rxye1991@gmail.com>
2019-11-20 02:02:04 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+  
+  
+  def initialize(title)
+    @title = title 
+  end
+  
+  attr_accessor :author, :page_count, :genre, :title
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+  
+end
+
+  
+
+
+book1 = Book.new("And Then There Were None")
+
 
 
